f387b036cd55512301d3cac8e8c019af3af2386f
Derek Foundoulis <derek@foundoulis.io>
2019-03-14 12:55:28 -0600

    87 done.

--- done/p087.py
+++ done/p087.py
@@ -20,3 +20,19 @@ def gen_primes():
             primes[previous_prime] = i
             yield primes[previous_prime]
         i += 1
+
+total = 50000000
+set_of_ints = set()
+for square in gen_primes():
+    for cube in gen_primes():
+        for fourth in gen_primes():
+            current = fourth**4 + cube**3 + square**2
+            if current > total:
+                break
+            else:
+                set_of_ints.add(current)
+        if square**2 + cube**3 > total:
+            break
+    if square**2 > total:
+        break
+print(len(set_of_ints))
